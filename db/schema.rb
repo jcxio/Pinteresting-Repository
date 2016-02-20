e351e4dcecb2d1c46ff254fb6aa2d8cf0ac82839
Oystein Lokken <oysteinlokken@gmail.com>
2016-02-20 15:19:26 +0800

    Added names to users+ name validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160214032300) do
+ActiveRecord::Schema.define(version: 20160220065331) do
 
   create_table "models", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 20160214032300) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
